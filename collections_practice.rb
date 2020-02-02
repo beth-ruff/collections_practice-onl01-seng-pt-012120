e6363041ffd9210016a5112d2c085a9361c280d5
Beth Ruff <beth.ruff2@gmail.com>
2020-02-02 00:35:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,3 +16,15 @@ def sort_array_desc(x)
   end
 end
 
+sort_array_char_count(y)
+  y = ["dogs", "cat", "Horses"]
+  y.sort do |a, b|
+    if a.length == b.length
+      0 
+    elsif a.length < b.length 
+      -1
+    elsif a.length > b.length
+      1 
+    end
+  end
+end
